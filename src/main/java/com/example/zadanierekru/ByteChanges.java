b7440182a1a4bec89e408b714fa32ea14895d13e
pgajewski95 <pgajewski95@wp.pl>
2019-09-11 18:59:48 +0200

    first inits

---
+++ src/main/java/com/example/zadanierekru/ByteChanges.java
@@ -0,0 +1,44 @@
+package com.example.zadanierekru;
+
+public class ByteChanges {
+
+    private String sciezka;
+    private String rozszerzenie;
+    private byte bajt_wejsciowy;
+    private byte bajt_wyjsciowy;
+
+
+
+
+    public String getSciezka() {
+        return sciezka;
+    }
+
+    public void setSciezka(String sciezka) {
+        this.sciezka = sciezka;
+    }
+
+    public String getRozszerzenie() {
+        return rozszerzenie;
+    }
+
+    public void setRozszerzenie(String rozszerzenie) {
+        this.rozszerzenie = rozszerzenie;
+    }
+
+    public byte getBajt_wejsciowy() {
+        return bajt_wejsciowy;
+    }
+
+    public void setBajt_wejsciowy(byte bajt_wejsciowy) {
+        this.bajt_wejsciowy = bajt_wejsciowy;
+    }
+
+    public byte getBajt_wyjsciowy() {
+        return bajt_wyjsciowy;
+    }
+
+    public void setBajt_wyjsciowy(byte bajt_wyjsciowy) {
+        this.bajt_wyjsciowy = bajt_wyjsciowy;
+    }
+}
---
+++ src/main/java/com/example/zadanierekru/ZadanierekruApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanierekru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanierekruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanierekruApplication.class, args);
+    }
+
+}
